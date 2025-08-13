62484f7a4e998cd18480641af36ef9230d1d122e
Sun <wanglt-123@qq.com>
2025-08-13 10:24:54 +0800

    添加theme.json和manifest.config.ts

---
+++ manifest.config.ts
@@ -0,0 +1,78 @@
+import { defineManifestConfig } from '@uni-helper/vite-plugin-uni-manifest';
+
+export default defineManifestConfig({
+  name: '',
+  appid: '__UNI__9F51105',
+  description: '',
+  versionName: '1.0.0',
+  versionCode: '100',
+  transformPx: false,
+  uniStatistics: {
+    enable: false
+  },
+  'app-plus': {
+    darkmode: true,
+    themeLocation: 'theme.json',
+    usingComponents: true,
+    nvueStyleCompiler: 'uni-app',
+    compilerVersion: 3,
+    splashscreen: {
+      alwaysShowBeforeRender: true,
+      waiting: true,
+      autoclose: true,
+      delay: 0
+    },
+    modules: {},
+    distribute: {
+      android: {
+        permissions: [
+          '<uses-permission android:name="android.permission.CHANGE_NETWORK_STATE"/>',
+          '<uses-permission android:name="android.permission.MOUNT_UNMOUNT_FILESYSTEMS"/>',
+          '<uses-permission android:name="android.permission.VIBRATE"/>',
+          '<uses-permission android:name="android.permission.READ_LOGS"/>',
+          '<uses-permission android:name="android.permission.ACCESS_WIFI_STATE"/>',
+          '<uses-feature android:name="android.hardware.camera.autofocus"/>',
+          '<uses-permission android:name="android.permission.ACCESS_NETWORK_STATE"/>',
+          '<uses-permission android:name="android.permission.CAMERA"/>',
+          '<uses-permission android:name="android.permission.GET_ACCOUNTS"/>',
+          '<uses-permission android:name="android.permission.READ_PHONE_STATE"/>',
+          '<uses-permission android:name="android.permission.CHANGE_WIFI_STATE"/>',
+          '<uses-permission android:name="android.permission.WAKE_LOCK"/>',
+          '<uses-permission android:name="android.permission.FLASHLIGHT"/>',
+          '<uses-feature android:name="android.hardware.camera"/>',
+          '<uses-permission android:name="android.permission.WRITE_SETTINGS"/>'
+        ]
+      },
+
+      ios: {},
+
+      sdkConfigs: {}
+    }
+  },
+
+  quickapp: {},
+
+  'mp-weixin': {
+    appid: 'wxcb5333b0dd39379d',
+    setting: {
+      urlCheck: false
+    },
+    usingComponents: true,
+    darkmode: true,
+    themeLocation: 'theme.json'
+  },
+  'mp-alipay': {
+    usingComponents: true
+  },
+  'mp-baidu': {
+    usingComponents: true
+  },
+  'mp-toutiao': {
+    usingComponents: true
+  },
+  h5: {
+    darkmode: true,
+    themeLocation: 'theme.json'
+  },
+  vueVersion: '3'
+});
