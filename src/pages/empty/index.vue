4e022897de31f4dd96f09665e382dc84eb245bdb
Sun <wanglt-123@qq.com>
2026-01-28 10:00:43 +0800

    feat: 添加空页面和我的页面
    添加了空页面作为占位页面
    
    添加了我的页面包含以下功能:
    - 暗黑模式切换开关
    - 主题色选择功能
    - 使用ActionSheet展示主题色选项
    - 集成useTheme组合式API进行主题管理

---
+++ src/pages/empty/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="text">空白</view>
+</template>
+
+<script setup lang="ts">
+definePage({
+  name: 'empty',
+  style: {
+    navigationBarTitleText: '空白页'
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
